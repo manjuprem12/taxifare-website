5b376e59b4b4e47d058bf9c37fb2962d3ff9eff1
Manju Prem <manjupremp@gmail.com>
2025-03-07 14:53:14 +0100

    My first website

--- app.py
+++ app.py
@@ -6,31 +6,31 @@ import requests
 # TaxiFareModel front
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
+# st.markdown('''
+# Remember that there are several ways to output content into your web page...
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
+# Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+# ''')
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+# '''
+# ## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
+# 1. Let's ask for:
+# - date and time
+# - pickup longitude
+# - pickup latitude
+# - dropoff longitude
+# - dropoff latitude
+# - passenger count
+# '''
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
+# '''
+# ## Once we have these, let's call our API in order to retrieve a prediction
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+# See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
+# 🤔 How could we call our API ? Off course... The `requests` package 💡
+# '''
 
 url = 'https://taxifare.lewagon.ai/predict'
 
@@ -38,16 +38,16 @@ if url == 'https://taxifare.lewagon.ai/predict':
 
     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-'''
+# '''
 
-2. Let's build a dictionary containing the parameters for our API...
+# 2. Let's build a dictionary containing the parameters for our API...
 
-3. Let's call our API using the `requests` package...
+# 3. Let's call our API using the `requests` package...
 
-4. Let's retrieve the prediction from the **JSON** returned by the API...
+# 4. Let's retrieve the prediction from the **JSON** returned by the API...
 
-## Finally, we can display the prediction to the user
-'''
+# ## Finally, we can display the prediction to the user
+# '''
 
 col1, col2 = st.columns(2)
 
